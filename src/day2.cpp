88b5fcd96a5457c6f6437e539966b802174a35cc
Morgan Strom <morgan.e.strom@gmail.com>
2020-12-04 22:29:19 +0100

    solves first task of day2

--- src/day2.cpp
+++ src/day2.cpp
@@ -18,15 +18,25 @@ std::vector<std::string> readFileToStringVector(std::string path) {
 int day2(std::string inputPath) {
 	std::vector<std::string> lines = readFileToStringVector(inputPath);
 	int numberOfLines = lines.size();
-	std::regex regexp("-");
+	std::regex regexp("([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)");
 	std::smatch m;
+	int numberOfValidPasswords = 0;
 
 	for (int i = 0; i < numberOfLines; i++) {
 		std::regex_search(lines[i], m, regexp);
-		std::cout << m.prefix().str() <<"\n";
-		// Todo: extract max number, letter and code
-		// Todo: count number of letters
-		// Todo: compare number of letters to min and max
+		int lower = std::stoi(m.str(1));
+		int upper = std::stoi(m.str(2));
+		char letter = m.str(3)[0];
+		std::string password = m.str(4);
+		int numberOfLetters = 0;
+		for (std::size_t j = 0; j < password.size(); j++) {
+			if (password[j] == letter) numberOfLetters++;
+		}
+
+		if ((numberOfLetters >= lower) && (numberOfLetters <= upper)) {
+			numberOfValidPasswords++;
+		}
 	}
+	std::cout << "First task: " << numberOfValidPasswords << std::endl;
 	return 0;
 }
