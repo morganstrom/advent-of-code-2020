b20cd2970834c233ab61696bed2c0f4186ac32d2
Morgan Ström <morgan.e.strom@gmail.com>
2020-12-05 01:03:23 +0100

    solves second task for day 3

--- src/day3.cpp
+++ src/day3.cpp
@@ -3,21 +3,27 @@
 #include <vector>
 #include "utils.h"
 
-int day3(std::string inputPath) {
-	std::vector<std::string> lines = readFileToStringVector(inputPath);
-	int numberOfRows = lines.size();
-	int numberOfCols = lines[0].size();
-	int row = 0;
+long countTrees(std::vector<std::string> map, int stepsRight, int stepsDown) {
+	int numberOfRows = map.size();
+	int numberOfCols = map[0].size();
 	int col = 0;
-	int numberOfTrees = 0;
-	for (int i = 0; i < numberOfRows; i++) {
-		row += 1;
-		col += 3;
+	long numberOfTrees = 0;
+	for (int row = 0; row < numberOfRows; ) {
+		row += stepsDown;
+		col += stepsRight;
 		if (col >= numberOfCols) {
 			col = col % numberOfCols;
 		}
-		if (lines[row][col] == '#') numberOfTrees++;
+		if (map[row][col] == '#') numberOfTrees++;
 	}
-	std::cout << "First task: " << numberOfTrees << std::endl;
+	return numberOfTrees;
+}
+
+int day3(std::string inputPath) {
+	std::vector<std::string> lines = readFileToStringVector(inputPath);
+	long firstTask = countTrees(lines, 3, 1);
+	long secondTask = firstTask * countTrees(lines, 1, 1) * countTrees(lines, 5, 1) * countTrees(lines, 7, 1) * countTrees(lines, 1, 2);
+	std::cout << "First task: " << firstTask << std::endl;
+	std::cout << "Second task: " << secondTask << std::endl;
 	return 0;
 }
