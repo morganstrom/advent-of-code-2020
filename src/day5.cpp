b764e9d321c1765db740a77c62528aa31cab761f
Morgan Ström <morgan.e.strom@gmail.com>
2020-12-09 10:05:08 +0100

    solves second task for day 5

--- src/day5.cpp
+++ src/day5.cpp
@@ -5,8 +5,11 @@
 
 int day5(std::string inputPath) {
 	std::vector<std::string> lines = readFileToStringVector(inputPath);
+	std::sort(lines.begin(), lines.end(), std::less<std::string>());
 	int numberOfBoardingPasses = lines.size();
 	int maxSeatID = 0;
+	int lastCol = 0;
+	int missingSeatID;
 	for (int i = 0; i < numberOfBoardingPasses; i++) {
 		int rowsLeft = 128;
 		int row = 0;
@@ -19,8 +22,11 @@ int day5(std::string inputPath) {
 			if (lines[i][j] == 'R') col = col + colsLeft;
 		}
 		int seatID = row * 8 + col;
+		if ((col - lastCol) > 1) missingSeatID = seatID - 1;
 		if (seatID > maxSeatID) maxSeatID = seatID;
+		lastCol = col;
 	}
 	std::cout << "First task: " << maxSeatID << std::endl;
+	std::cout << "Second task: " << missingSeatID << std::endl;
 	return 0;
 }
